989f29f96e49bd98bd3719e4c142e41c52f3a048
Stagiaires11 <Stagiaires11P@ad.fms-ea.com>
2022-06-08 09:20:01 +0200

    first commit

---
+++ src/main/java/com/fms/springEx1/SpringEx1Application.java
@@ -0,0 +1,13 @@
+package com.fms.springEx1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEx1Application.class, args);
+	}
+
+}
